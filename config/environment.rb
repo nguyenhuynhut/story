7ee195d878f2660c06f8074c86c888e01ffd9598
root <root@huynhut-Inspiron-N5010.(none)>
2011-05-19 11:00:19 +0700

    delete

--- config/environment.rb
+++ config/environment.rb
@@ -5,15 +5,7 @@ require 'mini_magick'
 StorySystem::Application.initialize!
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.content_type = "text/html"
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain => "gmail.com",
-  :authentication => 'plain',
-  :user_name => "benbo.nguyen@gmail.com",
-  :password => "hakimngoc",
-  :enable_starttls_auto => true
-}
-BUCKET = 'en_shootsheet_test'
+mails
+
 ENV['RECAPTCHA_PUBLIC_KEY'] = '6LenbsISAAAAAIIHxs6lrS-BmvysURpDqijUVzpJ'
 ENV['RECAPTCHA_PRIVATE_KEY'] = '6LenbsISAAAAALv1X8BUCuHM_tGzREGAfOr36zTf '
